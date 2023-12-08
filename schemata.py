be93d12b39e351b33f2306679b947374f7a97058
lefthand67 <lefthand67@yandex.ru>
2023-12-08 20:12:13 +0500

    role relataion added

--- schemata.py
+++ schemata.py
@@ -1,20 +1,26 @@
 # relations and their schemas
 relations = {
+    "author": [
+        ("id", "SERIAL"),
+        ("name", "VARCHAR(256) UNIQUE"),
+        ("PRIMARY KEY", "(id)"),
+    ],
+    "role": [
+        ("id", "SERIAL"),
+        ("name", "VARCHAR(64) UNIQUE"),
+        ("PRIMARY KEY", "(id)"),
+    ],
     "language": [
         ("id", "SERIAL"),
         ("name", "VARCHAR(64) UNIQUE"),
         ("PRIMARY KEY", "(id)"),
     ],
-    "author": [
-        ("id", "SERIAL"),
-        ("name", "VARCHAR(128) UNIQUE"),
-        ("PRIMARY KEY", "(id)"),
-    ],
     "book": [
         ("id", "SERIAL"),
-        ("title", "VARCHAR(256)"),
+        ("title", "VARCHAR(512)"),
         ("year", "INTEGER"),
         ("author_id", "INTEGER REFERENCES author(id) ON DELETE CASCADE"),
+        ("role_id", "INTEGER REFERENCES role(id) ON DELETE CASCADE"),
         ("language_id", "INTEGER REFERENCES language(id) ON DELETE CASCADE"),
         ("UNIQUE", "(title, year, language_id)"),
         ("PRIMARY KEY", "(id)"),
